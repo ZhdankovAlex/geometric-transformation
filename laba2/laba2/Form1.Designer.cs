a20fbe8f1d75b4f26cc81d49fc6aae1466985daf
ZhdankovAlex <47736968+ZhdankovAlex@users.noreply.github.com>
2020-08-14 10:52:12 +0300

    Add files via upload
    geometric transformation of figure

---
+++ laba2/laba2/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace laba2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button15 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button17);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.button15);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button16);
+            this.panel1.Controls.Add(this.button13);
+            this.panel1.Controls.Add(this.button14);
+            this.panel1.Controls.Add(this.button11);
+            this.panel1.Controls.Add(this.button12);
+            this.panel1.Controls.Add(this.button9);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(647, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(164, 458);
+            this.panel1.TabIndex = 0;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(94, 315);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(66, 20);
+            this.textBox4.TabIndex = 32;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(0, 61);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(67, 23);
+            this.button6.TabIndex = 31;
+            this.button6.Text = "OX =";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1, 90);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(160, 23);
+            this.button5.TabIndex = 30;
+            this.button5.Text = "// Y=X";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(4, 432);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(156, 23);
+            this.button17.TabIndex = 29;
+            this.button17.Text = "RESTART";
+            this.button17.UseVisualStyleBackColor = true;
+            this.button17.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 416);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 13);
+            this.label4.TabIndex = 28;
+            this.label4.Text = "RETURN TO START";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(4, 315);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(64, 20);
+            this.textBox3.TabIndex = 27;
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(4, 342);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(158, 23);
+            this.button15.TabIndex = 26;
+            this.button15.Text = "<TURN>";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1, 299);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(164, 13);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "относительно указанной точки";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 198);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 13);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Поворот на указанный градус";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(4, 214);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(156, 20);
+            this.textBox1.TabIndex = 16;
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(4, 240);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(158, 23);
+            this.button16.TabIndex = 14;
+            this.button16.Text = "<TURN>";
+            this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(94, 148);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(67, 23);
+            this.button13.TabIndex = 13;
+            this.button13.Text = "OY --";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(1, 148);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(67, 23);
+            this.button14.TabIndex = 12;
+            this.button14.Text = "OY ++";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(94, 119);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(67, 23);
+            this.button11.TabIndex = 11;
+            this.button11.Text = "OX --";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(1, 119);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(67, 23);
+            this.button12.TabIndex = 10;
+            this.button12.Text = "OX ++";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(93, 61);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(67, 23);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "OY ||";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(93, 32);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(67, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "OY - down";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(0, 32);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(67, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "OY - up";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(0, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(67, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "OX - left";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(93, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "OX - right";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(1, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 460);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(822, 477);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Лаба2";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ laba2/laba2/Form1.cs
@@ -0,0 +1,350 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laba2
+{
+    public partial class Form1 : Form
+    {
+        bool power = false;
+
+        const int additiveX = 320;
+        const int additiveY = 230;
+             
+        List<int> A = new List<int>() { 0, 0, 0 };
+        List<int> B = new List<int>() { 0, 0, 0 };
+        List<int> C = new List<int>() { 0, 0, 0 };
+        List<int> D = new List<int>() { 0, 0, 0 };
+        List<int> E = new List<int>() { 0, 0, 0 };
+        List<int> F = new List<int>() { 0, 0, 0 };
+        List<int> G = new List<int>() { 0, 0, 0 };
+        List<int> H0 = new List<int>() { 0, 0, 0 };
+        List<int> H1 = new List<int>() { 0, 0, 0 };
+        List<int> H2 = new List<int>() { 0, 0, 0 };
+        List<int> H3 = new List<int>() { 0, 0, 0 };
+
+        int shift = 50;
+
+        Pen p = new Pen(Color.Black, 1);
+
+        Graphics drawArea;
+        public Form1()
+        {
+            InitializeComponent();
+            drawArea = pictureBox1.CreateGraphics();
+        }
+        //draw figure by points
+        private void Draw(List<int> a, List<int> b, List<int> c, List<int> d,
+                          List<int> e, List<int> f, List<int> g, List<int> h0,
+                          List<int> h1, List<int> h2, List<int> h3)
+        {
+            drawArea.FillRectangle(Brushes.White, 0, 0, pictureBox1.Width, pictureBox1.Height);
+           
+            drawArea.DrawLine(p, 0, (int)(pictureBox1.Height/2), 
+                             (int)(pictureBox1.Width), (int)(pictureBox1.Height / 2));
+            drawArea.DrawLine(p, (int)(pictureBox1.Width/2), 0,
+                             (int)(pictureBox1.Width/2), (int)(pictureBox1.Height));
+            p = new Pen(Color.Red, 1);
+            drawArea.DrawLine(p, 90, (int)(pictureBox1.Height), (int)(pictureBox1.Width) - 90, 0);
+
+            p = new Pen(Color.Black, 1);
+            drawArea.DrawLine(p, a[0] + additiveX, additiveY - a[1], b[0] + additiveX, additiveY - b[1]);
+            drawArea.DrawLine(p, b[0] + additiveX, additiveY - b[1], c[0] + additiveX, additiveY - c[1]);
+            drawArea.DrawLine(p, c[0] + additiveX, additiveY - c[1], d[0] + additiveX, additiveY - d[1]);
+            drawArea.DrawLine(p, d[0] + additiveX, additiveY - d[1], e[0] + additiveX, additiveY - e[1]);
+            drawArea.DrawLine(p, e[0] + additiveX, additiveY - e[1], f[0] + additiveX, additiveY - f[1]);
+            drawArea.DrawLine(p, f[0] + additiveX, additiveY - f[1], g[0] + additiveX, additiveY - g[1]);
+            drawArea.DrawLine(p, g[0] + additiveX, additiveY - g[1], a[0] + additiveX, additiveY - a[1]);
+
+            Point H0 = new Point(h0[0] + additiveX, additiveY - h0[1]);
+            Point H1 = new Point(h1[0] + additiveX, additiveY - h1[1]);
+            Point H2 = new Point(h2[0] + additiveX, additiveY - h2[1]);
+            Point H3 = new Point(h3[0] + additiveX, additiveY - h3[1]);
+            Point[] curvePoints = {H0, H1, H2, H3};
+            float tension = 1.0F;
+            System.Drawing.Drawing2D.FillMode aFillMode = System.Drawing.Drawing2D.FillMode.Alternate;
+
+            drawArea.DrawClosedCurve(p, curvePoints.ToArray(), tension, aFillMode);
+        }
+        //initialization
+        private void button17_Click(object sender, EventArgs e)
+        {
+            power = true;
+
+            A[0] = 20;
+            A[1] = 110;
+            A[2] = 1;
+
+            B[0] = A[0] + 50;
+            B[1] = A[1] - 10;
+            B[2] = A[2];
+
+            //верхняя точка "короны"
+            C[0] = A[0] + 70;
+            C[1] = A[1] + 20;
+            C[2] = 1;
+
+            D[0] = A[0] + 90;
+            D[1] = A[1] - 10;
+            D[2] = 1;
+
+            E[0] = A[0] + 140;
+            E[1] = A[1];
+            E[2] = 1;
+
+            F[0] = A[0] + 120;
+            F[1] = A[1] - 30;
+            F[2] = 1;
+
+            G[0] = A[0] + 20;
+            G[1] = A[1] - 30;
+            G[2] = 1;
+
+            H0[0] = A[0] + 70;
+            H0[1] = A[1] - 30;
+            H0[2] = 1;
+
+            H1[0] = A[0] + 40;
+            H1[1] = A[1] - 60;
+            H1[2] = 1;
+
+            H2[0] = A[0] + 70;
+            H2[1] = A[1] - 90;
+            H2[2] = 1;
+
+            H3[0] = A[0] + 100;
+            H3[1] = A[1] - 60;
+            H3[2] = 1;
+
+            Draw(A, B, C, D, E, F, G, H0, H1, H2, H3);
+        }
+        //multiplicate method
+        List<int> Multiplicate(List<int>point, double[,]matrix)
+        {
+            List<int> result = new List<int>() { 0, 0, 0 };
+            for(int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    result[i] += (int)(point[j] * matrix[j, i]);
+                }
+            }
+            return result;
+        }
+        //transform
+        private void Transform(List<int> a, List<int> b, List<int> c,
+                               List<int> d, List<int> e, List<int> f,
+                               List<int> g, List<int> h0, List<int> h1,
+                               List<int> h2, List<int> h3, double[,] transform)
+        {
+            A = Multiplicate(a, transform);
+            B = Multiplicate(b, transform);
+            C = Multiplicate(c, transform);
+            D = Multiplicate(d, transform);
+            E = Multiplicate(e, transform);
+            F = Multiplicate(f, transform);
+            G = Multiplicate(g, transform);
+            H0 = Multiplicate(h0, transform);
+            H1 = Multiplicate(h1, transform);
+            H2 = Multiplicate(h2, transform);
+            H3 = Multiplicate(h3, transform);
+        }
+        //to_left
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (power)
+            {
+                double[,] transform = new double[3, 3] { { 1, 0, 0 }, { 0, 1, 0 }, { -shift, 0, 1 } };
+
+                Transform(A, B, C, D, E, F, G, H0, H1, H2, H3, transform);
+
+                Draw(A, B, C, D, E, F, G, H0, H1, H2, H3);
+            }
+        }
+        //to_right
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (power)
+            {
+                double[,] transform = new double[3, 3] { { 1, 0, 0 }, { 0, 1, 0 }, { shift, 0, 1 } };
+
+                Transform(A, B, C, D, E, F, G, H0, H1, H2, H3, transform);
+
+                Draw(A, B, C, D, E, F, G, H0, H1, H2, H3);
+            }
+        }
+        //to_up
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (power)
+            {
+                double[,] transform = new double[3, 3] { { 1, 0, 0 }, { 0, 1, 0 }, { 0, shift, 1 } };
+
+                Transform(A, B, C, D, E, F, G, H0, H1, H2, H3, transform);
+
+                Draw(A, B, C, D, E, F, G, H0, H1, H2, H3);
+            }
+        }
+        //to_down
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (power)
+            {
+                double[,] transform = new double[3, 3] { { 1, 0, 0 }, { 0, 1, 0 }, { 0, -shift, 1 } };
+
+                Transform(A, B, C, D, E, F, G, H0, H1, H2, H3, transform);
+
+                Draw(A, B, C, D, E, F, G, H0, H1, H2, H3);
+            }
+        }
+        //mirror_OY
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (power)
+            {
+                double[,] transform = new double[3, 3] { { 1, 0, 0 }, { 0, -1, 0 }, { 0, 0, 1 } };
+                                
+                Transform(A, B, C, D, E, F, G, H0, H1, H2, H3, transform);
+
+                Draw(A, B, C, D, E, F, G, H0, H1, H2, H3);
+            }
+        }
+        //mirror_OX
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (power)
+            {
+                double[,] transform = new double[3, 3] { { -1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } };
+
+                Transform(A, B, C, D, E, F, G, H0, H1, H2, H3, transform);
+
+                Draw(A, B, C, D, E, F, G, H0, H1, H2, H3);
+            }
+        }
+        //mirror_X=Y
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (power)
+            {
+                double[,] transform = new double[3, 3] { { 0, 1, 0 }, { 1, 0, 0 }, { 0, 0, 1 } };
+
+                Transform(A, B, C, D, E, F, G, H0, H1, H2, H3, transform);
+
+                Draw(A, B, C, D, E, F, G, H0, H1, H2, H3);
+            }
+        }
+        //scale_X++
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if (power)
+            {
+                double[,] transform = new double[3, 3] { { 2, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } };
+
+                Transform(A, B, C, D, E, F, G, H0, H1, H2, H3, transform);
+
+                Draw(A, B, C, D, E, F, G, H0, H1, H2, H3);
+            }
+        }
+        //scale_X--
+        private void button11_Click(object sender, EventArgs e)
+        {
+            if (power)
+            {
+                double[,] transform = new double[3, 3] { { 0.5, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } };
+
+                Transform(A, B, C, D, E, F, G, H0, H1, H2, H3, transform);
+
+                Draw(A, B, C, D, E, F, G, H0, H1, H2, H3);
+            }
+        }
+        //scale_Y++
+        private void button14_Click(object sender, EventArgs e)
+        {
+            if (power)
+            {
+                double[,] transform = new double[3, 3] { { 1, 0, 0 }, { 0, 2, 0 }, { 0, 0, 1 } };
+
+                Transform(A, B, C, D, E, F, G, H0, H1, H2, H3, transform);
+
+                Draw(A, B, C, D, E, F, G, H0, H1, H2, H3);
+            }
+        }
+        //scale_Y--
+        private void button13_Click(object sender, EventArgs e)
+        {
+            if (power)
+            {
+                double[,] transform = new double[3, 3] { { 1, 0, 0 }, { 0, 0.5, 0 }, { 0, 0, 1 } };
+
+                Transform(A, B, C, D, E, F, G, H0, H1, H2, H3, transform);
+
+                Draw(A, B, C, D, E, F, G, H0, H1, H2, H3);
+            }
+        }
+        //sin_angle
+        static double MySinus(double angle)
+        {
+            return (Math.Sin((angle / 180D) * Math.PI));
+        }
+        //cos_angle
+        static double MyCosinus(double angle)
+        {
+            if (angle % 90 == 0 && angle % 180 != 0) return 0;
+            return (Math.Cos((angle / 180D) * Math.PI));
+        }
+        //rotate_00
+        private void button16_Click(object sender, EventArgs e)
+        {
+             if (power)
+            {
+                double alpha = float.Parse(textBox1.Text);
+                double[,] transform = new double[3, 3] { { MyCosinus(alpha), MySinus(alpha), 0 }, { -MySinus(alpha), MyCosinus(alpha), 0 }, { 0, 0, 1 } };
+
+                Transform(A, B, C, D, E, F, G, H0, H1, H2, H3, transform);
+
+                Draw(A, B, C, D, E, F, G, H0, H1, H2, H3);
+            }
+        }
+        //rotate_XY
+        private void button15_Click(object sender, EventArgs e)
+        {            
+            if (power)
+            {
+                int x_point = int.Parse(textBox3.Text);
+                int y_point = int.Parse(textBox4.Text);
+                double alpha = float.Parse(textBox1.Text);
+                
+                double[,] transform1 = new double[3, 3]
+                { { 1, 0, 0 }, { 0, 1, 0 }, { -x_point, -y_point, 1 } };
+
+                double[,] transform2 = new double[3, 3] 
+                { { MyCosinus(alpha), MySinus(alpha), 0 }, 
+                { -MySinus(alpha), MyCosinus(alpha), 0 }, 
+                { 0, 0, 1 } };
+
+                double[,] transform3 = new double[3, 3]
+                { { 1, 0, 0 }, { 0, 1, 0 }, { x_point, y_point, 1 } };
+
+                A = Multiplicate(Multiplicate(Multiplicate(A, transform1), transform2), transform3);
+                B = Multiplicate(Multiplicate(Multiplicate(B, transform1), transform2), transform3);
+                C = Multiplicate(Multiplicate(Multiplicate(C, transform1), transform2), transform3);
+                D = Multiplicate(Multiplicate(Multiplicate(D, transform1), transform2), transform3);
+                E = Multiplicate(Multiplicate(Multiplicate(E, transform1), transform2), transform3);
+                F = Multiplicate(Multiplicate(Multiplicate(F, transform1), transform2), transform3);
+                G = Multiplicate(Multiplicate(Multiplicate(G, transform1), transform2), transform3);
+                H0 = Multiplicate(Multiplicate(Multiplicate(H0, transform1), transform2), transform3);
+                H1 = Multiplicate(Multiplicate(Multiplicate(H1, transform1), transform2), transform3);
+                H2 = Multiplicate(Multiplicate(Multiplicate(H2, transform1), transform2), transform3);
+                H3 = Multiplicate(Multiplicate(Multiplicate(H3, transform1), transform2), transform3);
+
+                Draw(A, B, C, D, E, F, G, H0, H1, H2, H3);
+            }
+        }
+    }
+}
